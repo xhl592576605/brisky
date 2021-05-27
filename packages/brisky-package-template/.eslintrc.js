17bb5a7877b6404280777efd9377c1753378de12
xuhuale <xuhuale@star-net.cn>
2021-05-27 16:16:58 +0800

    新增 brisky-util 工具库

--- packages/brisky-package-template/.eslintrc.js
+++ packages/brisky-package-template/.eslintrc.js
@@ -4,6 +4,13 @@ module.exports = {
     node: true
   },
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,//也就是ES6语法支持的意思
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true
+    }
+  },
   plugins: [
     '@typescript-eslint',
   ],
