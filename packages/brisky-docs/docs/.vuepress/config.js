0a37f87ef75cc25ac7071f7a12afec6e7c4b83cc
xhl592576605 <592576605@qq.com>
2021-06-29 00:25:59 +0800

    @brisky/docs 初始化文档的框架

---
+++ packages/brisky-docs/docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'Brisky',
+  description: 'Brisky 前端框架',
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+    ['link', { rel: 'icon', href: '/images/photo.jpg' }],
+    ['link', { rel: 'manifest', href: '/images/photo.jpg' }],
+    ['link', { rel: 'apple-touch-icon', href: '/images/photo.jpg' }],
+    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache'}],
+    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache,must-revalidate'}],
+    ['meta', { 'http-quiv': 'expires', cotent: '0'}]
+  ],
+  serviceWorker: true, // 是否开启 PWA
+  base: '/', // 部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块是否显示行号
+  },
+  themeConfig: {
+    nav:[ // 导航栏配置   
+    ],
+    sidebar: require("./sidebar"),
+    displayAllHeaders: true,
+    //sidebarDepth: 2
+  }
+};
